5d0330370db1aadd14962ae80bdee100ae4e7013
botechiaeri <botechiaeri@gmail.com>
2022-08-05 00:55:01 -0300

    make it better

--- src/controllers/mainController.js
+++ src/controllers/mainController.js
@@ -1,7 +1,29 @@
 let controller = {
     home: (req, res)=>{
-        res.render('index.ejs' , { titlePage : "Front End Test"})
+   res.render('index.ejs')
+        /*     const fs = require('fs');
+        const path = require('path');
+        const postFilePath = path.join(__dirname, '/../data/postData.json');
+        const postArray = JSON.parse(fs.readFileSync(postFilePath, 'utf-8'));
+        res.render('index.ejs' , { 
+            titlePage : "Front End Test",
+            user: postArray[postArray.lenght].nameU,
+            post: postArray[postArray.lenght].content,
+        })*/
+    },
+      /*  create: (req, res)=>{
+        const fs = require('fs');
+        const path = require('path');
+        const postFilePath = path.join(__dirname, '/../data/postData.json');
+        const postArray = JSON.parse(fs.readFileSync(postFilePath, 'utf-8'));
+        let newPost = {
+        this.nameU: req.body.namePost,
+           this.content: req.body.postBody
+            };
+          postArray.push(newPost);
+          fs.writeFileSync(__dirname + '/../data/telosDB.json', JSON.stringify(postArray, null, ' '));
+          res.redirect('/');
+      },*/
     }
-}
 
 module.exports = controller;
--- src/routes/main.js
+++ src/routes/main.js
@@ -3,5 +3,5 @@ const express = require('express')
 const router = express.Router()
 
 router.get('/', mainController.home)
-
+//router.post('/', mainController.create)
 module.exports = router
